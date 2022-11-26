e03a30e0b12ef53becd222e05198e2ef5da6ac34
Khadidja <khadija.aitsiali@gmail.com>
2022-11-26 17:25:08 +0100

    services: re-done gsap script

---
+++ js/gsap.js
@@ -0,0 +1,44 @@
+// gsap animation
+
+// Overlay
+TweenMax.to(".first", 1.5, {
+  delay: .4,
+  top: "-100%",
+  ease: Expo.easeInOut
+});
+
+TweenMax.to(".second", 1.5, {
+  delay: .6,
+  top: "-100%",
+  ease: Expo.easeInOut
+});
+
+TweenMax.to(".third", 1.5, {
+  delay: .8,
+  top: "-100%",
+  ease: Expo.easeInOut
+});
+
+// Landing animation
+// let tl = gsap.timeline();
+
+// tl.from('hr', {
+//   duration: 2,
+//   width: 0
+// })
+
+// Cards animation
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".box-card");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length + 0),
+  ease: "linear",
+  scrollTrigger: {
+    trigger: ".box",
+    pin: true,
+    scrub: -2,
+    end: () => "+=" + document.querySelector(".box").offsetWidth
+  }
+});
